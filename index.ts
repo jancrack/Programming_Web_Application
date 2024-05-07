2087edd8e06f8dfabb16919f10e525c8e4864d39
Nikolay Kulev <nkulev71@gmail.com>
2024-05-07 18:24:52 +0300

    after mysql

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 // @ts-ignore
 import express, {Application, json, Request, Response} from 'express';
-import mysql2 from "mysql";
+import mysql2 from "mysql2";
 
 const app: Application = express();
 
 app.use(json())
 
-app.get("/api", (req: Request, res: Response) => {
-    res.send({
-        message: "1"
-    })
+app.get("/", async (req: Request, res: Response) => {
+    const conn = await mysql2.createPool({
+        host: "localhost",
+        database: "web_application",
+        user: "root",
+        password: ""
+    }).promise();
+
+    const [rows] = await conn.query("SELECT * FROM cars");
+    console.log({rows});
+
+    res.send(rows)
 })
 app.put("/", (req: Request, res: Response) => {
     res.send({
